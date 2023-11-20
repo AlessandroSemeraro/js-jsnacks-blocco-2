e8e9a1fcb258b040d4940171f031d97de3b6042e
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-20 11:40:31 +0100

    Inizializzazione

---
+++ cartella snack-3/js/script.js
@@ -0,0 +1,17 @@
+const userFirstWord = prompt('insert first word');
+
+const userSecondWord = prompt('insert second word');
+
+function(userFirstWord, userSecondWord){
+    if( userFirstWord.lenght = userSecondWord.lenght){
+        console.log(userFirstWord, userSecondWord);
+
+    }else if (userFirstWord.lenght > userSecondWord.lenght){
+        console.log(userFirstWord)
+    }
+
+    else if( userFirstWord.lenght < userSecondWord.lenghy){
+        console.log(userSecondWord)
+    }
+
+}
