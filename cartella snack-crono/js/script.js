e06e7458cb68087824d174ab838d9ff755a0e858
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-23 12:21:53 +0100

    Inizializzazione

---
+++ cartella snack-crono/js/script.js
@@ -0,0 +1,9 @@
+const buttonStart = document.getElementById('btn-start');
+console.log(buttonStart)
+
+const buttonEnd = document.getElementById('btn-end');
+console.log(buttonEnd)
+
+const timeNumber = document.querySelector('body h1');
+console.log(timeNumber)
+
